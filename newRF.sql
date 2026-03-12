8e225bfc4c376933efad2016acd1c8bf5a9c9c5f
elvegr <it@elve.gr>
2026-03-12 12:45:14 +0200

    Add SQL script to update GXSTRINGFIELD4
    Update GXSTRINGFIELD4 in GXTRADER for specific GXID.

---
+++ newRF.sql
@@ -0,0 +1,8 @@
+
+-----ΚΩΔΙΚΟΣ ΣΥΝΑΛΛΑΣΣΟΜΕΝΟΥ
+Update GXTRADER Set GXSTRINGFIELD4=[dbo].[DSPRI18Generator](1, '20580')
+
+--------------ΑΦΜ ΚΩΔΙΚΟΣ ΣΥΝΑΛΛΑΣΣΟΜΕΝΟΥ
+ Where GXID = (select gxid from gxtrader where GXTIN = '800627321')
+
+ ----------EXECUTE
